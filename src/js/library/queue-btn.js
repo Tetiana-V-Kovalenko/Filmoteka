7942e8cb85cbca846dfbc6d6d5c3345df96fb188
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2022-12-05 14:40:40 +0200

    Initial commit

---
+++ src/js/library/queue-btn.js
@@ -0,0 +1,25 @@
+import { refs } from './refs-library';
+import { getQueue } from '../init-form';
+
+import { MovieAPI } from '../movie-API';
+import { appendCardMarkupLibrary } from './append-card-markup-library';
+const movieApi = new MovieAPI();
+// const STORAGE_QUEUE = 'queue-state';
+
+export async function onQueueClick() {
+  refs.gallery_library.innerHTML = '';
+  refs.library_empty.classList.remove("library_empty", "watched_empty");
+  refs.spinner.classList.remove('hidden');
+  refs.libraryWatched.classList.remove('header-library__btn-active');
+  refs.libraryQueue.classList.add('header-library__btn-active');
+  const arrSelectedQueue = getQueue();
+  if (arrSelectedQueue.length === 0){
+    refs.library_empty.classList.add("queue_empty")
+  }
+  const arrFetchPromises = arrSelectedQueue.map(el =>
+    movieApi.getMovieById(el)
+  );
+  const responseOfFetchPromises = await Promise.all(arrFetchPromises);
+  appendCardMarkupLibrary(responseOfFetchPromises);
+  refs.spinner.classList.add('hidden');
+}
---
+++ src/js/refs-homepage.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.main'),
+  container: document.getElementById('tui-pagination-container'),
+  notifySearchFailure: document.querySelector('.search-notify-failure'),
+  spinner: document.querySelector('.lds-roller'),
+  backdropEl: document.querySelector('.backdrop'),
+  galleryEl: document.querySelector('.gallery'),
+  studentLink: document.querySelector('.footer__accent-text'),
+  studentModal: document.querySelector('.modal-students'),
+  backdropStudents: document.querySelector('.backdrop_students'),
+};
